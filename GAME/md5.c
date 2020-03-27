51f25adef1ace0fa8424fc7d28336736e2d49996
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-03-27 13:10:41 +0100

    Revert "funcionando ok"
    This reverts commit eb6457bf6049c0339d73d68c69e284188f27bc6f.

--- GAME/md5.c
+++ GAME/md5.c
@@ -8,27 +8,22 @@
 
 char *string = "teste";
 
-unsigned char * smd5();
+unsigned char smd5();
 
 int main(){
 	
 	int i=0;
 	
-	unsigned char *result=smd5();
+	unsigned char *result;
+	result = malloc(sizeof(unsigned char)*MD5_DIGEST_LENGTH);
+	//unsigned char result[MD5_DIGEST_LENGTH];
 	
-
-	if(result){
-		
-		for(i = 0; i < MD5_DIGEST_LENGTH; i++){
-			printf("%02x", result[i]);
-		}
-		
-		// Libera espaco 
-		free(result);
+	*result=smd5();
+	
+	for(i = 0; i < MD5_DIGEST_LENGTH; i++){
+		printf("%02x", result[i]);
 	}
-	
 
-	
 	
 	return 1;
 }
@@ -36,23 +31,23 @@ int main(){
 	
 
 	
-unsigned char * smd5(){
+unsigned char smd5(){
 	
 	int i;
 	
-	unsigned char * resultado  = malloc(sizeof(unsigned char)*MD5_DIGEST_LENGTH);
-	
+	unsigned char *result;
+	result = malloc(sizeof(unsigned char)*MD5_DIGEST_LENGTH);
 	//unsigned char result[MD5_DIGEST_LENGTH];
 	
-	MD5(string, strlen(string), resultado);
+	MD5(string, strlen(string), result);
 	
 	for(i = 0; i < MD5_DIGEST_LENGTH; i++){
-		printf("%02x", resultado[i]);
+		printf("%02x", result[i]);
 	}
 	
 	printf("\n");
 	
-	return resultado;
+	return *result;
 }
 	
 	
