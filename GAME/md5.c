3b9c9e760a3790632f185bdc0b4db31f03ba88f5
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-03-27 12:28:12 +0100

    criando md5

---
+++ GAME/md5.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <mysql.h>
+#include <string.h>
+//#include <unistd.h>
+//#include <stdlib.h>
+
+#include <openssl/md5.h>
+
+char *string = "teste";
+
+unsigned char smd5();
+
+int main(){
+	
+	int i=0;
+	
+	//char *result;
+	
+	unsigned char result[MD5_DIGEST_LENGTH];
+	
+	*result=smd5();
+	
+	for(i = 0; i < MD5_DIGEST_LENGTH; i++){
+		printf("%02x", result[i]);
+	}
+
+	
+	return 1;
+}
+
+	
+
+	
+unsigned char smd5(){
+	
+	int i;
+	
+	unsigned char *result;
+	result = malloc(sizeof(unsigned char)*MD5_DIGEST_LENGTH);
+	//unsigned char result[MD5_DIGEST_LENGTH];
+	
+	MD5(string, strlen(string), &result[0]);
+	
+	for(i = 0; i < MD5_DIGEST_LENGTH; i++){
+		printf("%02x", result[i]);
+	}
+	
+	printf("\n");
+	
+	return *result;
+}
+	
+	
+
+	
+	
