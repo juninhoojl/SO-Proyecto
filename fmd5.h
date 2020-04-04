7f4ec98964b9d3ab198ba985e2d4e95b811d7bba
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-04-04 18:40:11 +0200

    resolvido problema incluir libs
    Resolvido Error #include nested too deeply

--- fmd5.h
+++ fmd5.h
@@ -1,3 +1,7 @@
+#ifndef _FMD5_H_
+#define _FMD5_H_
+
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -10,3 +14,5 @@ unsigned char * smd5();
 int maina();
 
 
+
+#endif
--- servidor.h
+++ servidor.h
@@ -1,9 +1,28 @@
-#include <stdio.h>
-#include <mysql.h>
+#ifndef _SERVIDOR_H_
+#define _SERVIDOR_H_
+
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <pthread.h>
+#include "basedados.h"
+
+
+struct thread_args
+{
+	int a;
+	double b;
+};
+
+void *AtenderCliente (void *args_void);
+
+
+
+
+#endif
+
 
