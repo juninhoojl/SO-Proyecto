f43e2cfcdec90b2e8b8d0338532e9560227f8e60
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-04-05 12:10:15 +0200

    correcao pt 2

--- fmd5.c
+++ fmd5.c
@@ -4,33 +4,22 @@
 // Lembrar de desalocar depois de usar
 int main(){
 
-	char string[]= "asdfghjk";
 
-	char *result;
+	char *result = malloc((MD5_DIGEST_LENGTH*2+1)*sizeof( char));
 	
-	result = malloc((MD5_DIGEST_LENGTH*2+1)*sizeof( char));
-	
-	result=smd5(string,result);
+	result = smd5("asdfghjk",result);
 
 	printf("%s\n",result);
 	
 	free(result);
 
-	return 1;
+	return 0;
 }
 
-void f(char *dst,unsigned char *src,size_t src_len)
-{
+void fconv(char *dst,unsigned char *src,size_t src_len){
 	while (src_len--) 
 		dst += sprintf(dst,"%02x",*src++);
 	*dst = '\0';
-}
-
-int mainfa(){
-	
-	unsigned char btemp[]= { 0x04, 0x0a, 0xd0, 0xc2,0x34,0x3c,0xed,0x06,0x10,0x11};
-	char representation[sizeof(btemp)*2+1];
-	f(representation,btemp,sizeof(btemp));    
 }
 
 
@@ -39,7 +28,8 @@ char * smd5(char * palavra, char * digest){
 	unsigned char *result;
 	result = malloc(sizeof(unsigned char)*MD5_DIGEST_LENGTH);
 	MD5(palavra, strlen(palavra), result);
-	f(digest,result,2*sizeof(result));
+	fconv(digest,result,2*sizeof(result));
+	free(result);
 	return digest;
 }
 
